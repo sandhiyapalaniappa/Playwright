9600c4c4f551a8d29ffb60644e8db0c31393a3f5
sandhiyapalaniappa <60645308+sandhiyapalaniappa@users.noreply.github.com>
2026-09-02 10:21:04 +0530

    Add files via upload

---
+++ learnDataTypes.js
@@ -0,0 +1,21 @@
+
+
+let firstName="Sandhiya"
+console.log(firstName)
+console.log(typeof firstName)
+
+let companyName="ABCD"
+console.log(companyName)
+console.log(typeof companyName)
+
+let mobileNumber=7598681529
+console.log(mobileNumber)
+console.log(typeof mobileNumber)
+
+let isAutomation=true
+console.log(isAutomation)
+console.log(typeof isAutomation)
+
+let hasPlayWright=null
+console.log(hasPlayWright)
+console.log(typeof hasPlayWright)
